808b281eb0282775b3781663876f70a6789a2f69
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2017-05-18 21:57:54 +0300

    Extracting POS and Entities.

--- src/main/java/com/hashtag/context/index/HashtagIndex.java
+++ src/main/java/com/hashtag/context/index/HashtagIndex.java
@@ -1,7 +1,8 @@
 package com.hashtag.context.index;
 
 import com.hashtag.context.stream.Twitter;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.ngram.NGramTokenizer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
@@ -11,6 +12,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
+import twitter4j.HashtagEntity;
 import twitter4j.TwitterException;
 
 import java.io.File;
@@ -22,6 +24,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,7 +32,7 @@ public class HashtagIndex {
     private FSDirectory index;
     private IndexWriter indexWriter;
     private IndexWriterConfig indexConfig;
-    private StandardAnalyzer analyzer;
+    private Analyzer analyzer;
 
     private TrackingIndexWriter trackingIndexWriter;
     private ReferenceManager<IndexSearcher> searcherManager;
@@ -41,7 +44,12 @@ public class HashtagIndex {
             t.delete();
         }
 
-        this.analyzer = new StandardAnalyzer();
+        this.analyzer = new Analyzer() {
+            @Override
+            protected TokenStreamComponents createComponents(String s) {
+                return new TokenStreamComponents(new NGramTokenizer(1, 4));
+            }
+        };
         this.indexConfig = new IndexWriterConfig(this.analyzer);
         this.index = FSDirectory.open(Paths.get(new File("hashtags.lcn").toURI()));
         this.indexWriter = new IndexWriter(this.index, this.indexConfig);
@@ -56,9 +64,9 @@ public class HashtagIndex {
 
     }
 
-    public void appendDocument(String keywords) throws IOException {
+    public void appendDocument(String term) throws IOException {
         Document document = new Document();
-        document.add(new TextField("term", keywords, TextField.Store.YES));
+        document.add(new TextField("term", term, TextField.Store.YES));
         this.indexWriter.addDocument(document);
     }
 
@@ -71,63 +79,61 @@ public class HashtagIndex {
         QueryParser qp = new QueryParser("term", this.analyzer);
         Query query = qp.parse(qp.escape(hashtag));
 
-        TopDocs docs = searcher.search(query, 10);
+        TopDocs docs = searcher.search(query, 5);
 
         Set<String> terms = new HashSet<>();
         for (ScoreDoc doc : docs.scoreDocs) {
-            terms.add(searcher.doc(doc.doc).get("term"));
+            terms.add(searcher.doc(doc.doc).get("term").toLowerCase());
         }
         searcherManager.release(searcher);
 
+        if (terms.size() == 1) {
+            String[] extras = hashtag.toLowerCase().split(terms.toArray()[0].toString());
+            terms.addAll(Arrays.asList(extras));
+        }
         return terms;
     }
 
-    public static void main(String args[]) throws IOException, InterruptedException, ParseException {
+    public static void main(String args[]) throws IOException, InterruptedException, ParseException, TwitterException {
         HashtagIndex index = new HashtagIndex();
-        Thread writer = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Twitter.stream().forEach(status -> {
-                        try {
-                            Markable markable = MarkableExtractor.extractMarkables(status);
-                            Set<String> tokens = new HashSet<>(markable.getNouns());
-                            tokens.addAll(markable.getVerbs());
-                            tokens.addAll(markable.getEntities());
-
-                            for (String token : tokens) {
-                                index.appendDocument(token.toLowerCase().trim());
-                            }
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (CertificateException e) {
-                            e.printStackTrace();
-                        } catch (NoSuchAlgorithmException e) {
-                            e.printStackTrace();
-                        } catch (KeyStoreException e) {
-                            e.printStackTrace();
-                        } catch (UnrecoverableKeyException e) {
-                            e.printStackTrace();
-                        } catch (KeyManagementException e) {
-                            e.printStackTrace();
-                        } catch (URISyntaxException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (TwitterException e) {
-                    e.printStackTrace();
+        Twitter.stream().forEach(status -> {
+            try {
+                Markable markable = MarkableExtractor.extractMarkables(status);
+                Set<String> tokens = new HashSet<>(markable.getEntities());
+                tokens.addAll(markable.getNouns());
+                tokens.addAll(markable.getVerbs());
+                for (String token : tokens) {
+                    if (token.startsWith("#") || token.startsWith("http")) {
+                        continue;
+                    }
+                    token = token.replaceAll("\n", "");
+                    token = token.toLowerCase();
+                    token = token.trim();
+                    index.appendDocument(token);
                 }
+
+                for (HashtagEntity entity : status.getHashtagEntities()) {
+                    System.out.println(((Runtime.getRuntime().freeMemory() + Runtime.getRuntime().maxMemory() - Runtime.getRuntime().totalMemory()) / 1024) + "\t#" + entity.getText() + ": " + index.contextFor(entity.getText().toLowerCase()));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (CertificateException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (UnrecoverableKeyException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (KeyStoreException e) {
+                e.printStackTrace();
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
-        };
-
-        writer.start();
-        Thread.sleep(60000);
-
-        System.out.println(index.contextFor("brexit"));
-
+        });
     }
 
 }
